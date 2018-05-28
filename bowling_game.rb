45c169bb1e1caf6b3466d889656207a882d7e500
carotene512 <karoten512@gmail.com>
2018-05-29 07:49:43 +0900

    spareの実装途中

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,10 +1,21 @@
 class BowlingGame
   def initialize
     @score = 0
+    @spare_flg
   end
 
   def record_shot(pins)
     @score = @score + pins
+
+    if @spare_flg
+      @score = @score + pins
+      @spare_flg = false
+    end
+
+    if @score == 10
+      @spare_flg = true
+    end
+
   end
 
   def score
--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -11,9 +11,19 @@ class BowlingGameTest < MiniTest::Unit::TestCase
     assert_equal 0, @game.score
   end
 
+  ## あ、すごい。いままでのデグレが検出されるぞ。
   def test_すべての投球で１つだけ倒した
     record_many_shot(20, 1)
     assert_equal 20, @game.score
+  end
+
+  def test_3_7spare_4
+    @game.record_shot(3)
+    @game.record_shot(7) # spare 3 + 7 + 4 + (4)
+    @game.record_shot(4)
+
+    record_many_shot(17, 0)
+    assert_equal 18, @game.score
   end
 
   private
